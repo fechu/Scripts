d36f5f54e42227dacce76496cf2eddc753d30b0e
Sandro Meier <sandro.meier@fidelisfactory.ch>
2012-03-06 10:59:34 +0100

    Fixed formatting.

--- PHP/Count.php
+++ PHP/Count.php
@@ -10,15 +10,15 @@ class Count extends SM\Script\Script {
      */
     protected function help()
     {
-	$help ="
-	Use this script to count everything in texts.
-	
-	Usage: ./Count.php [-t <text>]
-	
-	Options
-	    -t	The text you want to use.
-	    ";
-	return $help;
+		$help ="
+		Use this script to count everything in texts.
+		
+		Usage: ./Count.php [-t <text>]
+		
+		Options
+		    -t	The text you want to use.
+		    ";
+		return $help;
     }
     
     
@@ -27,45 +27,45 @@ class Count extends SM\Script\Script {
      */
     protected function getOptions()
     {
-	$short_options = array(
-	    "t:" => false
-	);
+		$short_options = array(
+	    	"t:" => false
+		);
 	
-	return array($short_options, array());
+		return array($short_options, array());
     }
     
     /**
      * The script. Count things in the given text.
      */
     protected function script()
-    {
-	// Get the text.
-	$text = $this->options['t'];
+   	{
+		// Get the text.
+		$text = $this->options['t'];
+		
+		// Count the characters
+		$chars = strlen($text);
+		$this->write("Characters: " . $chars);	
+		
+		// Count the characters without whitespaces
+		$plainText = preg_replace("/\s*/i", "", $text);
+		$charsWithout = strlen($plainText);
+		$this->write("Characters without whitespaces: " . $charsWithout);
+		
+		// Count the words
+		$words = preg_match_all("/\w+/", $text, $matches);
+		$this->write("Words: ". $words);
+		
+		// Count the lines
+		$lines = substr_count($text, "\n") + 1;
+		$this->write("Lines: " . $lines);
+		
 	
-	// Count the characters
-	$chars = strlen($text);
-	$this->write("Characters: " . $chars);	
-	
-	// Count the characters without whitespaces
-	$plainText = preg_replace("/\s*/i", "", $text);
-	$charsWithout = strlen($plainText);
-	$this->write("Characters without whitespaces: " . $charsWithout);
-	
-	// Count the words
-	$words = preg_match_all("/\w+/", $text, $matches);
-	$this->write("Words: ". $words);
-	
-	// Count the lines
-	$lines = substr_count($text, "\n") + 1;
-	$this->write("Lines: " . $lines);
-	
-
-	
-	$this->write("Found Characters:");
-	foreach(count_chars($text, 1) as $i => $val)
-	{
-	    $this->write(chr($i) . ": " . $val);
-	}
+		
+		$this->write("Found Characters:");
+		foreach(count_chars($text, 1) as $i => $val)
+		{
+		    $this->write(chr($i) . ": " . $val);
+		}
     }
 }
 
